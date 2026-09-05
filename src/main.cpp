f6d658f02fdc99f61176b0f4294d6630e1d5ad19
Sljivoa14 <amarsljivo7@gmail.com>
2026-09-05 21:51:36 +0200

    ESP initialization and setup for firmware

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS   15
+#define TFT_DC   2
+#define TFT_RST  4
+#define TFT_BL   32
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    pinMode(TFT_BL, OUTPUT);
+    digitalWrite(TFT_BL, HIGH);
+
+    SPI.begin(TFT_SCLK, -1, TFT_MOSI, TFT_CS);
+
+    tft.init(170, 320);
+    tft.setRotation(1);
+
+    tft.fillScreen(ST77XX_BLACK);
+
+    tft.setTextColor(ST77XX_WHITE);
+    tft.setTextSize(3);
+    tft.setCursor(30, 50);
+    tft.println("HELLO!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
