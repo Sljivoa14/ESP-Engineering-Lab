b85bb353cfb3c2887e5ce123b59819336e853ca5
Sljivoa14 <amarsljivo7@gmail.com>
2026-09-17 22:06:57 +0200

    Frame capturing works on esp32 oled screen

--- src/main.cpp
+++ src/main.cpp
@@ -121,9 +121,8 @@ A NEW FILE STRUCTRE:
 └── ESP-Engineering-Lab-Camera/
     ├── platformio.ini
     └── src/
-        └── main.cpp     <-     Problem:  FRAME CAPTURE FAILED
-                                Current evidence: S3 CAMERA NODE ALIVE but frame capture fails.  Null-pointer-style crash during camera setup.
-                                Next objective: TAKE A SINGLE PICTURE/ frane capturing
+        └── main.cpp     <-    FRAME CAPTURE WORKS !!!
+                                NEXT PART: CONECTNING THE ESPWROOM WITH THE ESP32S3
 
 
 NETWORK LAB
